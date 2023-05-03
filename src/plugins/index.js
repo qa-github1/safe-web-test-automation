412f5b002e1ec48f71a991644ace3b42421af888
sumejja-tracker <59477886+sumejja-tracker@users.noreply.github.com>
2023-05-03 22:22:31 +0200

    adjustments for allure reports

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -39,7 +39,7 @@ function getEnvironmentConfig(config) {
     configJson["env"] = environmentData;
     configJson["baseUrl"] = environmentData["baseUrl"];
     configJson["orgNum"] = config["env"]["orgNum"];
-    configJson["env"]["allureResultsPath"] = "report/allure-results-" + config["env"]["orgNum"];
+    configJson["env"]["allureResultsPath"] = "report/" + config["env"]["orgNum"] + "/allure-results";
     configJson["env"]["allure"] = true;
 
     console.log('CONFIG PARAMETERS: ' + JSON.stringify(configJson))
