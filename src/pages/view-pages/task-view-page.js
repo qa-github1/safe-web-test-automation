4ae514ebeaf382c8dde22d77c97fa4741374dd70
Sumejja <qa+github1@trackerproducts.com>
2023-06-07 19:37:49 +0200

    Update task-view-page.js

--- src/pages/view-pages/task-view-page.js
+++ src/pages/view-pages/task-view-page.js
@@ -4,9 +4,15 @@ import BaseViewPage from "../base-pages/base-view-page";
 //************************************ ELEMENTS ***************************************//
 
 let
-    noteInput = e => cy.get('[ng-model="vm.newTaskNote"]')
+    noteInput = e => cy.get('[ng-model="vm.newTaskNote"]'),
+    searchInput = e => cy.get('#searchtask'),
+    sortingArrow = columnTitle => cy.get('.order').first(),
+    tableColumn_header = columnTitle => cy.get('thead').contains(columnTitle),
+    tableColumn_header_arrowUp = columnTitle => cy.get('thead').contains(columnTitle).parent().find('.order'),
+    tableColumn_header_sortingArrow = columnTitle => cy.get('thead').contains(columnTitle).parent().find('.order')
 
-export default class TaskViewPage extends BaseViewPage {
+
+    export default class TaskViewPage extends BaseViewPage {
 
     constructor() {
         super()
@@ -17,6 +23,25 @@ export default class TaskViewPage extends BaseViewPage {
     verify_Task_View_page_is_open() {
         this.toastMessage().should('not.exist');
         noteInput().should('be.visible');
+        return this;
+    };
+
+    search_for_the_task(keyword) {
+        this.enterValue(searchInput, keyword)
+        return this;
+    };
+
+    sort_by_descending_order(columnTitle) {
+        sortingArrow().parents('th').invoke('text').then((text) => {
+            //cy.log('Data is sorted by  '+ text)
+            if (!text.includes(columnTitle)) {
+                tableColumn_header(columnTitle).click()
+            }
+            this.pause(1)
+            this.click_element_if_has_a_class(tableColumn_header_sortingArrow(columnTitle), 'dropup')
+            tableColumn_header_sortingArrow(columnTitle).should('not.have.class', 'dropup')
+
+        });
         return this;
     };
 
