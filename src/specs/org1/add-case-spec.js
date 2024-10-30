5ef811d0a497c0c3760257be32133ba76e6e7b5e
qa-github1 <124904003+qa-github1@users.noreply.github.com>
2024-10-30 10:28:11 +0100

    moving permission update from 'Before' block to specific test where needed

--- src/specs/org1/add-case-spec.js
+++ src/specs/org1/add-case-spec.js
@@ -27,11 +27,9 @@ describe('Add Case', function () {
 
         before(function () {
             api.auth.get_tokens(orgAdmin);
-            // api.auto_disposition.edit(true);
-            // api.org_settings.set_Org_Level_Case_Number_formatting(false, false, false)
-            // api.users.update_current_user_settings(orgAdmin.id, C.currentDateTimeFormat)
-            api.permissions.update_ALL_permissions_for_an_existing_Permission_group(permissionGroup_regularUser, true, true, true, true)
-
+            api.auto_disposition.edit(true);
+            api.org_settings.set_Org_Level_Case_Number_formatting(false, false, false)
+            api.users.update_current_user_settings(orgAdmin.id, C.currentDateTimeFormat)
             api.auth.get_tokens(powerUser);
             api.users.update_current_user_settings(powerUser.id, C.currentDateTimeFormat)
         });
@@ -135,6 +133,7 @@ describe('Add Case', function () {
 
             api.auth.get_tokens(orgAdmin);
             api.permissions.assign_office_based_permissions_to_user(powerUser.id, office_1.id, permissionGroup_regularUser.id);
+            api.permissions.update_ALL_permissions_for_an_existing_Permission_group(permissionGroup_regularUser, true, true, true, true)
             api.permissions.set_CRUD_permissions_for_specific_entity_on_existing_Permission_group(permissionGroup_regularUser, C.perissionMatrixEntity.autoDispo, false, null, false, false, null)
             D.getNewCaseData();
             D.newCase.reviewDate = null
