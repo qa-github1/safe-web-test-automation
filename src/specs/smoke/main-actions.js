420af05d3948303cf6dd3f10e201e351cfdb4ca9
Sumejja <124904003+qa-github1@users.noreply.github.com>
2025-04-17 11:04:03 +0200

    Update main-actions.js

--- src/specs/smoke/main-actions.js
+++ src/specs/smoke/main-actions.js
@@ -127,7 +127,7 @@ describe('Case', function () {
 });
 describe('Item', function () {
 
-    it.only(
+    it(
         '*** Add/Edit/Search/MassUpdate Item ' +
         '*** Add/Search Item Note  ' +
         '*** Add/Search Item Media', function () {
