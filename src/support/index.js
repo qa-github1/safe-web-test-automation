983fa6d106259363cdfc01069ae7e63321dd074f
Sumejja <124904003+qa-github1@users.noreply.github.com>
2025-04-28 08:22:16 +0200

    Manual Console Log Hooking

--- src/support/index.js
+++ src/support/index.js
@@ -1,6 +1,5 @@
 import '@shelex/cypress-allure-plugin';
 import './commands'
-require('cypress-failed-log');
 
 const C = require('../fixtures/constants');
 const S = require('../fixtures/settings');
@@ -9,4 +8,18 @@ const ui = require('../pages/ui-spec');
 
 after(function() {
     cy.window().then(win => win.onbeforeunload = undefined );
+});
+
+Cypress.on('window:before:load', (win) => {
+    const originalConsoleError = win.console.error;
+    win.console.error = function (...args) {
+        // Log it to terminal
+        Cypress.log({
+            name: 'console.error',
+            message: args,
+        });
+
+        // Also keep default behavior
+        originalConsoleError.apply(win.console, args);
+    };
 });
