151dc26fef13156d85bcc7340e9f2f1527d7b5a6
Sumejja <124904003+qa-github1@users.noreply.github.com>
2025-05-06 13:38:26 +0200

    fixed edit-case #5

--- src/pages/view-pages/case-view-page.js
+++ src/pages/view-pages/case-view-page.js
@@ -68,7 +68,8 @@ export default class CaseViewPage extends BaseViewPage {
     //************************************ ACTIONS ***************************************//
 
     enter_Case_Number(caseNo) {
-        caseNumberInput_enabled().clear();
+       // caseNumberInput_enabled().clear();
+        caseNumberInput_enabled().invoke('val', '').trigger('input')
         caseNumberInput_enabled().should('have.class', 'ng-empty');
         caseNumberInput_enabled().type(caseNo);
         return this;
